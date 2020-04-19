975f6e3c888d7f9d4345c55dc770693188bf5637
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-04-19 22:13:53 +0200

    Acciones del módulo Products haciendo petición HTTP con Async y Await

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import productsModule from './modules/products'
+import products from './modules/products'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  state: {},
   modules: {
-    products: productsModule
+    products: products
   }
 })
