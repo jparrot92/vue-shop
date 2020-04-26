0e74688006e01b91951a3b9acd4fdf140f5e3346
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-04-26 13:47:49 +0200

    Iniciar el módulo Vuex del carrito de compras y desarrollar mutaciones

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import products from './modules/products'
+import cart from './modules/cart'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   modules: {
-    products: products
+    products,
+    cart
   }
 })
