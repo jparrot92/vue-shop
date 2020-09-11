9a8240ed13cae94cf54ce5fb73a80e891e2683ef
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-09-11 15:20:52 +0200

    Persistir Vuex en LocalStorage

--- src/store.js
+++ src/store.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import VuexPersistence from 'vuex-persist'
 import products from './modules/products'
 import cart from './modules/cart'
-
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: ['cart']
+})
 
 export default new Vuex.Store({
   state: {},
   modules: {
     products,
     cart
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
